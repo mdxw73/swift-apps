33cb4e860ca9f50ce0dfe8a3a504e71a23f82377
obiedz <obied.z@etoncollege.org.uk>
2020-01-16 13:14:48 +0000

    Push

--- TamagotchiApp/TamagotchiApp/Tamagotchi.swift
+++ TamagotchiApp/TamagotchiApp/Tamagotchi.swift
@@ -15,19 +15,21 @@ class Tamagotchi {
     private var age: Int
     private var weight: Int
     private var height: Int
-    private var happy: Bool
-    private var hungry: Bool
+    private var happy: Int
+    private var hungry: Int
     private var ill: Bool
     private var dirty: Bool
     private var dead: Bool
     
+    private var mealCount: Int = 3
+    
     init() {
         name = "Tammy"
         age = 0
-        weight = 1
-        height = 1
-        happy = true
-        hungry = true
+        weight = 5
+        height = 5
+        happy = 5
+        hungry = 5
         ill = false
         dirty = false
         dead = false
@@ -36,7 +38,7 @@ class Tamagotchi {
     func displayStats() -> String {
         return ("""
             Name: \(name)
-            Age: \(age)
+            Age: \(age) days
             Weight: \(weight)kg
             Height: \(height)"
             Happy: \(happy)
@@ -50,44 +52,66 @@ class Tamagotchi {
         return self.dead
     }
     
+    func getWeight() -> Int {
+        return self.weight
+    }
+    
     func feedMeal() {
-        if self.height < 10 && self.weight < 10 {
-            self.hungry = false
-            self.weight += 2
-            self.height += 1
+        if mealCount > 0 && self.hungry > 2 {
+            self.hungry -= 3
+            mealCount -= 1
             if Int.random(in: 1...5) == 1 {
                 self.ill = true
             }
+        } else {
+            if self.weight < 18 {
+                self.weight += 3
+            } else {
+                self.dead = true
+            }
+            if happy > 4 {
+                self.happy -= 5
+            }
+            mealCount = 3
         }
     }
     
     func feedSnack() {
-        if self.height < 10 && self.weight < 10 {
-            self.hungry = false
-            self.weight += 1
+        if mealCount > 0 && self.hungry > 0 {
+            self.hungry -= 1
+            mealCount -= 1
             if Int.random(in: 1...5) == 1 {
                 self.ill = true
             }
+        } else {
+            if weight < 18 {
+                self.weight += 1
+            } else {
+                self.dead = true
+            }
+            if happy > 4 {
+                self.happy -= 5
+            }
+            mealCount = 3
         }
     }
     
     func playGame() {
-        if self.weight > 1 && height > 1 {
-            self.happy = true
+        if self.weight > 1 && happy < 9 {
+            self.happy += 2
+            if self.weight > 1 {}
             self.weight -= 1
-            self.height -= 1
             if Int.random(in: 1...5) == 1 {
                 self.dirty = true
             }
+        } else {
+            self.ill = true
         }
     }
     
     func growUp() {
         self.age += 1
-        self.happy = false
-        self.hungry = true
-        self.ill = true
-        self.dirty = true
+        self.height += 1
         if Int.random(in: 1...5) == 1 && age > 10 {
             self.dead = true
         }
@@ -96,7 +120,9 @@ class Tamagotchi {
     func cleanUp() {
         self.dirty = false
         if Int.random(in: 1...5) == 1 {
-            self.hungry = true
+            if hungry < 6{
+                self.hungry += 5
+            }
         }
     }
     
--- TamagotchiApp/TamagotchiApp/ViewController.swift
+++ TamagotchiApp/TamagotchiApp/ViewController.swift
@@ -17,10 +17,18 @@ class ViewController: UIViewController {
             tamagotchiStatistics.text = tamagotchi?.displayStats()
         }
     }
+    var timer: Timer?
+    var ageTimer = 0
+    var mealTimer = 0
+    var playTimer = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tamagotchi = Tamagotchi()
+        ageTimer = 90
+        mealTimer = 60
+        playTimer = 30
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
     }
     
     @IBAction func feedMeal(_ sender: Any) {
@@ -38,11 +46,6 @@ class ViewController: UIViewController {
         display()
     }
     
-    @IBAction func growUp(_ sender: Any) {
-        tamagotchi?.growUp()
-        display()
-    }
-    
     @IBAction func cleanUp(_ sender: Any) {
         tamagotchi?.cleanUp()
         display()
@@ -55,7 +58,7 @@ class ViewController: UIViewController {
     
     @IBAction func rules(_ sender: Any) {
         let alertController = UIAlertController(title: "Rules", message:
-            "Get you tamagotchi as old as you can. It can only age when it's: happy, fed, well and clean. If you try to age it when it's not any of these things, it'll die. If it goes overweight (over 10kg) or gets too skinny (over 10 inches), then he'll die. You might also kill it by giving it medication, but that's the only way to make it healthy; it's your call!", preferredStyle: .alert)
+            "Get your tamagotchi as old as you can. It can only age when it's: happy, fed, well and clean. If you try to age it when it's not any of these things, it'll die. If it goes overweight (over 10kg) or gets too skinny (over 10 inches), then he'll die. You might also kill it by giving it medication, but that's the only way to make it healthy; it's your call!", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Continue", style: .default))
 
         self.present(alertController, animated: true, completion: nil)
@@ -65,6 +68,36 @@ class ViewController: UIViewController {
         tamagotchiStatistics.text = tamagotchi?.displayStats()
         if tamagotchi?.getHealth() == true {
             tamagotchiStatistics.text = "Your Tamagotchi Died"
+        }
+    }
+    
+    @objc func countdown() {
+        if ageTimer > 0 {
+            ageTimer -= 1
+        } else {
+            tamagotchi?.growUp()
+            playTimer = Int.random(in: 30...100)
+            display()
+        }
+        if mealTimer > 0 {
+            mealTimer -= 1
+        } else {
+            let alertController = UIAlertController(title: "Your Tamagotchi", message:
+                "I'm Hungry", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Okay", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+            playTimer = Int.random(in: 30...100)
+        }
+        if playTimer > 0 {
+            playTimer -= 1
+        } else {
+            let alertController = UIAlertController(title: "Your Tamagotchi", message:
+                "Let's Play Something", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Okay", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+            playTimer = Int.random(in: 30...100)
         }
     }
     
